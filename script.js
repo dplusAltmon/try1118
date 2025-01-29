9bef7a98eb2ddfffd2118c6fdf17b450e5c3e789
dplusAltmon <444293674@qq.com>
2025-01-29 21:21:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// 打开视频模态框的函数
+function openVideo(videoId) {
+    var modal = document.getElementById(videoId);
+    if (modal) {
+        modal.style.display = "block";
+    }
+}
 
+// 关闭视频模态框的函数
+function closeVideo(videoId) {
+    var modal = document.getElementById(videoId);
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
