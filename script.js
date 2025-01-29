41eaae70e7cd8354934ffad12f583a5a2fc75d8f
dplusAltmon <444293674@qq.com>
2025-01-29 21:21:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 打开视频模态框的函数
-function openVideo(videoId) {
+function openVideo(riize_video.mp4) {
     var modal = document.getElementById(videoId);
     if (modal) {
         modal.style.display = "block";
@@ -7,7 +7,7 @@ function openVideo(videoId) {
 }
 
 // 关闭视频模态框的函数
-function closeVideo(videoId) {
+function closeVideo(riize_video.mp4) {
     var modal = document.getElementById(videoId);
     if (modal) {
         modal.style.display = "none";
